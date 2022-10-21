0c5cb76c13d5dc08624137d11f2aba667fafc7a7
nastyaprieto-uab <nastya.prietouab@gmail,com>
2022-10-21 14:19:49 +0200

    Crear patró Singleton Toogle

--- timeTracker/src/TimeController.java
+++ timeTracker/src/TimeController.java
@@ -3,10 +3,25 @@ import java.util.Observable;
 
 public class TimeController extends Observable {
   public LocalDateTime dateTimeNow;
-
+  private static TimeController instance;
+   
+   
   private void tick() {
     dateTimeNow = LocalDateTime.now();
     setChanged();
     notifyObservers(dateTimeNow);
   }
+  
+  //Singleton Toogle pattern
+  private TimeController() {
+
+  }
+  public static TimeController getInstance() {
+        
+		if (instance == null) {
+			instance = new TimeController();
+		}
+		
+		return instance;
+    }
 }
