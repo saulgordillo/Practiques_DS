ae4758aa36df49317ef33800e88675e7fab08816
Hugo GB <hugogaben8.02@gmail.com>
2022-11-18 09:15:34 +0100

    small naming change

--- timeTracker/src/Project.java
+++ timeTracker/src/Project.java
@@ -7,12 +7,12 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Project extends Activity {
-  protected final List<Activity> activities;
+  protected final List<Activity> projects;
 
   public Project(boolean isRoot) {
     this.name = "root";
     this.projectFather = null;
-    this.activities = new LinkedList<>();
+    this.projects = new LinkedList<>();
 
     this.isRoot = isRoot;
   }
@@ -20,19 +20,19 @@ public class Project extends Activity {
   public Project(String name, Project father) {
     this.name = name;
     this.projectFather = father;
-    this.activities = new LinkedList<>();
+    this.projects = new LinkedList<>();
     if (father != null) {
       father.addChild(this);
     }
   }
 
   public void addChild(Activity child) {
-    this.activities.add(child);
+    this.projects.add(child);
   }
 
   public void calculateDuration() {
     this.duration = Duration.ofSeconds(0);
-    for (Activity activity : activities) {
+    for (Activity activity : projects) {
       //We are iterating through projects to sum every interval to get the duration of this task
       this.duration = this.duration.plus(activity.getDuration());
     }
@@ -43,7 +43,7 @@ public class Project extends Activity {
     JSONArray list = new JSONArray();
     JSONObject task = new JSONObject();
 
-    for (Activity activity : activities) {
+    for (Activity activity : projects) {
       if (activity instanceof Project) {
         list.put(((Project) activity).projectToJSON());
       } else if (activity instanceof Task) {
